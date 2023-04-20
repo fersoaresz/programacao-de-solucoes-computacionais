b737e6759d02016b5e036c6db5e4610b67a19374
fernando <fernan_fer_soar@hotmail.com>
2023-04-20 20:59:35 -0300

    atividade 1 qst 2

--- meuprimeiroprojetonoeclipse/src/atividade1/Q2.java
+++ meuprimeiroprojetonoeclipse/src/atividade1/Q2.java
@@ -1,5 +1,20 @@
 package atividade1;
+import java.util.Scanner;
 
 public class Q2 {
+	  public static void main(String[] args) {
+		    Scanner input = new Scanner(System.in);
 
-}
+		    System.out.print("digite o ano de nascimento: ");
+		    int anoNascimento = input.nextInt();
+
+		    System.out.print("digite o ano atual: ");
+		    int anoAtual = input.nextInt();
+
+		    int idadeAtual = anoAtual - anoNascimento;
+		    System.out.println("sua idade atual: " + idadeAtual);
+
+		    int idade2050 = 2050 - anoNascimento;
+		    System.out.println("sua idade em 2050: " + idade2050);
+		  }
+		}
--- meuprimeiroprojetonoeclipse/src/atividade1/Q22.java
+++ meuprimeiroprojetonoeclipse/src/atividade1/Q22.java
@@ -1,5 +1,18 @@
 package atividade1;
+import javax.swing.JOptionPane;
 
 public class Q22 {
+	  public static void main(String[] args) {
+		    String anoNascimentoStr = JOptionPane.showInputDialog("digite o ano de nascimento:");
+		    int anoNascimento = Integer.parseInt(anoNascimentoStr);
 
-}
+		    String anoAtualStr = JOptionPane.showInputDialog("digite o ano atual:");
+		    int anoAtual = Integer.parseInt(anoAtualStr);
+
+		    int idadeAtual = anoAtual - anoNascimento;
+		    JOptionPane.showMessageDialog(null, "sua idade atual: " + idadeAtual);
+
+		    int idade2050 = 2050 - anoNascimento;
+		    JOptionPane.showMessageDialog(null, "sua idade em 2050: " + idade2050);
+		  }
+		}
