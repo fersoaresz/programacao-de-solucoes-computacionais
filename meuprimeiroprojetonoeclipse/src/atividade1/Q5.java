b610565ad88f5af3a9c2620e40a808118ec5ba4d
fernando <fernan_fer_soar@hotmail.com>
2023-04-20 21:16:40 -0300

    atividade 1 qst5

--- meuprimeiroprojetonoeclipse/src/atividade1/Q5.java
+++ meuprimeiroprojetonoeclipse/src/atividade1/Q5.java
@@ -1,5 +1,18 @@
 package atividade1;
+import java.util.Scanner;
 
 public class Q5 {
+	  public static void main(String[] args) {
+		    Scanner input = new Scanner(System.in);
 
-}
+		    System.out.print("Digite a diagonal maior do losango: ");
+		    double diagonalMaior = input.nextDouble();
+
+		    System.out.print("Digite a diagonal menor do losango: ");
+		    double diagonalMenor = input.nextDouble();
+
+		    double area = (diagonalMaior * diagonalMenor) / 2;
+
+		    System.out.println("a area do losango é: " + area);
+		  }
+		}
--- meuprimeiroprojetonoeclipse/src/atividade1/Q55.java
+++ meuprimeiroprojetonoeclipse/src/atividade1/Q55.java
@@ -1,5 +1,15 @@
 package atividade1;
-
+import javax.swing.JOptionPane;
 public class Q55 {
+	  public static void main(String[] args) {
+		    String diagonalMaiorStr = JOptionPane.showInputDialog("Digite a diagonal maior do losango:");
+		    double diagonalMaior = Double.parseDouble(diagonalMaiorStr);
 
-}
+		    String diagonalMenorStr = JOptionPane.showInputDialog("Digite a diagonal menor do losango:");
+		    double diagonalMenor = Double.parseDouble(diagonalMenorStr);
+
+		    double area = (diagonalMaior * diagonalMenor) / 2;
+
+		    JOptionPane.showMessageDialog(null, "A área do losango é: " + area);
+		  }
+		}
