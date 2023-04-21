ec15bfb572d51e13fc5d62c6284dfedba263aee0
fernando <fernan_fer_soar@hotmail.com>
2023-04-20 21:14:25 -0300

    atividade 1 qst4

--- meuprimeiroprojetonoeclipse/src/atividade1/Q4.java
+++ meuprimeiroprojetonoeclipse/src/atividade1/Q4.java
@@ -1,5 +1,11 @@
 package atividade1;
-
+import java.util.Scanner;
 public class Q4 {
-
-}
+	  public static void main(String[] args) {
+		    Scanner input = new Scanner(System.in);
+		    System.out.print("digite o salario atual do funcionario: ");
+		    double salario = input.nextDouble();
+		    double novoSalario = salario * 1.25;
+		    System.out.println("O novo salario do funcionario é: R$ " + novoSalario);
+		  }
+		}
--- meuprimeiroprojetonoeclipse/src/atividade1/Q44.java
+++ meuprimeiroprojetonoeclipse/src/atividade1/Q44.java
@@ -1,5 +1,10 @@
 package atividade1;
-
+import javax.swing.JOptionPane;
 public class Q44 {
-
-}
+	  public static void main(String[] args) {
+		    String salarioString = JOptionPane.showInputDialog("digite o salario atual do funcionario:");
+		    double salario = Double.parseDouble(salarioString);
+		    double novoSalario = salario * 1.25;
+		    JOptionPane.showMessageDialog(null, "O novo salario do funcionario é: R$ " + novoSalario);
+		  }
+		}
