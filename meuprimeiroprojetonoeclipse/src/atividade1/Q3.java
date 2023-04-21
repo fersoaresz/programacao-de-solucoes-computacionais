9369f2be2315ce9253be168bf64058493d0f6727
fernando <fernan_fer_soar@hotmail.com>
2023-04-20 21:11:20 -0300

    atividade 1 qst3

--- meuprimeiroprojetonoeclipse/src/atividade1/Q3.java
+++ meuprimeiroprojetonoeclipse/src/atividade1/Q3.java
@@ -1,5 +1,17 @@
 package atividade1;
+import java.util.Scanner;
+public class Q3  {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-public class Q3 {
+        System.out.print("digite o valor do dolar em real: ");
+        double cotacao = input.nextDouble();
 
+        System.out.print("digite o valor em dolares: ");
+        double valorDolar = input.nextDouble();
+
+        double valorReal = cotacao * valorDolar;
+
+        System.out.println("valor em reais: R$" + valorReal);
+    }
 }
--- meuprimeiroprojetonoeclipse/src/atividade1/Q33.java
+++ meuprimeiroprojetonoeclipse/src/atividade1/Q33.java
@@ -1,5 +1,16 @@
 package atividade1;
+import javax.swing.JOptionPane;
 
 public class Q33 {
+    public static void main(String[] args) {
+        String cotacaoStr = JOptionPane.showInputDialog("digite o valor do dolar em real:");
+        double cotacao = Double.parseDouble(cotacaoStr);
 
+        String valorDolarStr = JOptionPane.showInputDialog("digite o valor em dolar:");
+        double valorDolar = Double.parseDouble(valorDolarStr);
+
+        double valorReal = cotacao * valorDolar;
+
+        JOptionPane.showMessageDialog(null, "valor em reais: R$" + valorReal);
+    }
 }
